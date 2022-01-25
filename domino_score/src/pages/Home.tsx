4b05f153c60b20c3f113c7d79507fb1f0cd9921b
Rodrigo Vitor Ribeiro <ribeiro.rvs@hotmail.com>
2022-01-25 17:38:52 -0300

    Create structure to pass in the tests

--- domino_score/src/pages/Home.tsx
+++ domino_score/src/pages/Home.tsx
@@ -5,7 +5,18 @@ import { layout } from '../styles/layout';
 export function Home() {
     return (
         <View style={layout.container}>
-            <Text>Open up App.tsx to start working on your app!</Text>
+            <View>
+                <View>
+                    <Text>Player 1</Text>
+                    <View></View>
+                    <Text>Player 2</Text>
+                </View>
+                <View>
+                    <Text>Player 1</Text>
+                    <View></View>
+                    <Text>Player 2</Text>
+                </View>
+            </View>
         </View>
     );
 }
